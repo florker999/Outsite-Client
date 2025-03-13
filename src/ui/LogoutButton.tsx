86773fe5fa61255152eff98073696ddc4fb3ca89
CloudfreundMaciek <88267476+florker999@users.noreply.github.com>
2025-03-13 10:47:48 +0100

    [fix] Logging out didn't redirect

--- src/ui/LogoutButton.tsx
+++ src/ui/LogoutButton.tsx
@@ -3,12 +3,14 @@
 import { logOut } from "@/lib/services/API";
 import { deleteSession } from "@/lib/services/Cookie";
 import { Button } from "@chakra-ui/react";
+import { redirect } from "next/navigation";
 import { LuLogOut } from "react-icons/lu";
 
 export default function LogoutButton() {
     const logout = async () => {
         await deleteSession(); 
-        await logOut();
+        redirect('/');
+        // await logOut();
     }
 
     return (
